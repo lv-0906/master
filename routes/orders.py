5a140eddf0fe25f37c1975a1180a8a64c87e4e9b
lv-0906 <liuwei@motern.com>
2025-09-01 10:01:28 +0800

    modified:   routes/orders.py

--- routes/orders.py
+++ routes/orders.py
@@ -12,3 +12,13 @@ def get_orders():
     cursor.close()
     conn.close()
     return jsonify(orders)
+@orders_bp.route("/createorder",methods=["post"])
+def create_order():
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    data = request.get_json()
+    cursor.execute("INSERT INTO orders(family_id,user_id,dishes,image_url)VALUES(%s,%s,%s,%s)",(data["familyid"],data["userid"],data["dishes"],data["image_url"]))
+    conn.commit()
+    cursor.close()
+    conn.close()
+    return jsonify({"status":"success"})
